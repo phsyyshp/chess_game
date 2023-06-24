cbba1e76f06df5bb1ca69fc79afb1b1711492c7a
phsyyshp <theoriginal169@gmail.com>
2023-06-24 04:36:30 -0400

    i

--- game.py
+++ game.py
@@ -3,21 +3,38 @@ import numpy as np
 from    utilities  import *
 class game:
     def __init__(self):
-        self.board_history = [bd.create_board()]
+        self.board_history = [bd.create_board().board]  
+        self.board = bd.create_board()
     def show(self):
         graphic_of_board = self.board_history[-1].show(is_print = 0)
         graph_of_remaining_time = self.remaining_time.show()
         graphe_of_captured_pieces = self
 
-        self.show)a
+#        self.showa
         self.board_history[-1].show()
         self.player1.show_captured_pieces()
-    def start_game(self, time_control):
-        board = self.board_history[-1]
+    def is_input_valid(self, user_input):
+        if is_algebraic_notation(user_input):
+            return self.board.is_movement_valid(algebraic_notation)
+        else:
+            return False
+
+    def get_movement_from_user(self):
+        is_input_valid = False
+        while not is_input_valid:
+            movement = input("movement:")
+            is_input_valid = self.is_input_valid()
+    def update_board(self, movement):
+        old_position, new_position = self.algebric_input_to_num(movement)
+        self.board.move_piece(old_position, new_position )
+        self.board_history.append(self.board.board)
+
+
+    def play_game(self, time_control):
         while not self.is_over():
            self.show()
            movement = self.get_movement_from_user()
            self.update_time()
-           board.update(movement)
+           self.update_board(movement)
            self.show()
-           self.board_history.append(board)
+           
