94595b579f8f536b8a77acc1ae5a2c11cad7ae33
phsyyshp <theoriginal169@gmail.com>
2023-06-27 19:36:54 -0400

    fixed bug in diagonal add slopes

--- individual_pieces.py
+++ individual_pieces.py
@@ -1,23 +1,20 @@
-from utilities import file_rank_to_row_col, is_coordinate_in_board, numeric_to_algebraic, diagonal_squares, horizontal_squares, vertical_squares
+from utilities import *
 import itertools
 class pawn:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'pawn'
     def is_in_starting_position(self):
-        position_row_column = file_rank_to_row_col(self.position_file_rank)
         if self.color == 'white':
-            return position_row_column[0] == 1
+            return self.position_row_column[0] == 1
         else:
-            return position_row_column[0] == 6
+            return self.position_row_column[0] == 6
 
     def show_forward_squares(self, amount, direction):
         color_to_sign = {'white': 1, 'black': -1}
-        position_row_column = file_rank_to_row_col(self.position_file_rank)
-        return (position_row_column[0] + color_to_sign[self.color] * amount, position_row_column[1] + direction * amount)
+        return (self.position_row_column[0] + color_to_sign[self.color] * amount, self.position_row_column[1] + direction * amount)
     def moves_in_range(self):
-        position_row_column = file_rank_to_row_col(self.position_file_rank)
         amounts = [1,2,1,1]
         directions = [0,0,-1, 1]
         if not self.is_in_starting_position():
@@ -25,46 +22,43 @@ class pawn:
             directions.pop(1)
         squares_in_range = [self.show_forward_squares(amount, direction) for amount, direction in zip(amounts, directions) if is_coordinate_in_board(self.show_forward_squares(amount, direction) )  ]
         return squares_in_range
-    def is_in_range(self, new_position_file_rank):
-        return file_rank_to_row_col(new_position_file_rank) in self.moves_in_range()
         
 class knight:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'knight'
     def moves_in_range(self):
-        position_row_column = algebraic_position(self.position_file_rank)
-        
+        return L_shaped_squares(self.position_row_column)
 class bishop:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'bishop'
     def moves_in_range(self):
-        print(self.position_file_rank)
-        return diagonal_squares(self.position_file_rank ) 
+        return diagonal_squares(self.position_row_column ) 
+    def get_path(self):
+        
         
 class rook:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'rook'
     def moves_in_range(self):
-        return horizontal_squares(self.position_file_rank) + vertical_squares(self.position_file_rank)
+        return horizontal_squares(self.position_row_column) + vertical_squares(self.position_row_column)
 class queen:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'queen'
     def moves_in_range(self):
-        return horizontal_squares(self.position_file_rank) + vertical_squares(self.position_file_rank) +  diagonal_squares(self.position_file_rank)
+        return horizontal_squares(self.position_row_column) + vertical_squares(self.position_row_column) +  diagonal_squares(self.position_row_column)
 class king:
-    def __init__(self, position_file_rank, color):
-        self.position_file_rank = position_file_rank
+    def __init__(self, position_row_column, color):
+        self.position_row_column = position_row_column
         self.color = color
         self.type = 'king'
     def moves_in_range(self):
-        position_row_column = file_rank_to_row_col(self.position_file_rank)
-        return horizontal_squares(self.position_file_rank, amount= 1) + vertical_squares(self.position_file_rank, amount= 1) +  diagonal_squares(self.position_file_rank, amount= 1)
+        return horizontal_squares(self.position_row_column, amount= 1) + vertical_squares(self.position_row_column, amount= 1) +  diagonal_squares(self.position_row_column, amount= 1)
 pp = pawn('a7','white')
