0fc1b2a34a4323167164061fdf9cfcc410051612
phsyyshp <theoriginal169@gmail.com>
2023-06-27 11:33:30 -0400

    improved var and func names

--- utilities.py
+++ utilities.py
@@ -1,72 +1,59 @@
 import numpy as np
 
-def algebraic_to_numeric(algebraic_position):
-    print(algebraic_position)
-    return (int(algebraic_position[1])-1,ord(algebraic_position[0])-97)
-def is_coordinate_in_board(numeric_position):
-    if numeric_position[0] in range(8) and numeric_position[1] in range(8):
+def file_rank_to_row_col(position_file_rank):
+    print(position_file_rank)
+    return (int(position_file_rank[1])-1,ord(position_file_rank[0])-97)
+
+def is_coordinate_in_board(position_row_column):
+    if position_row_column[0] in range(8) and position_row_column[1] in range(8):
         return True
     else:
         return False
-def numeric_to_algebraic(numeric_position):
-    return chr(numeric_position[1]+97)+str(numeric_position[0]+1)
-def diagonal_squares(position, amount = 8):
-    numeric_position = algebraic_to_numeric(position)
-    min_coor = min(numeric_position[0], numeric_position[1])
-    if  min_coor-amount < 0:
+
+def row_col_to_file_rank(position_row_column):
+    return chr(position_row_column[1]+97)+str(position_row_column[0]+1)
+
+def find_bounds(input_val, amount):
+    if  input_val-amount < 0:
         lower_bound = 0
     else:
-        lower_bound = min_coor - amount 
-    if amount + min_coor > 7:
+        lower_bound = input_val - amount 
+    if amount + input_val > 7:
         upper_bound = 7
     else:
-        upper_bound = amount + min_coor
+        upper_bound = amount + input_val
+    return lower_bound, upper_bound
+
+def diagonal_squares(position_file_rank, amount = 8):
+    position_row_column = file_rank_to_row_col(position_file_rank)
+    min_cord = min(position_row_column[0], position_row_column[1])
+    lower_bound, upper_bound = find_bounds(min_cord, amount)
+    
     indices = np.arange(lower_bound, upper_bound+1)
     indices_to_raveled = np.stack([indices, indices])
     raveled_indices = np.ravel_multi_index(indices_to_raveled, (8,8))
     binary_mat = np.zeros((1,64))
     binary_mat[0][raveled_indices] = 1
     binary_mat = binary_mat.reshape((8,8))
-    binary_mat[numeric_position[0]][numeric_position[1]] = 0
+    binary_mat[position_row_column[0]][position_row_column[1]] = 0
     return  binary_mat
 
-def horizontal_squares(position, amount = 8):
-    numeric_position = algebraic_to_numeric(position)
+def horizontal_squares(position_file_rank, amount = 8):
+    position_row_column = file_rank_to_row_col(position_file_rank)
     binary_mat = np.zeros((8,8))
-
-    if  numeric_position[1]-amount < 0:
-        lower_bound = 0
-    else:
-        lower_bound = numeric_position[1] - amount 
-    if amount + numeric_position[1] > 7:
-        upper_bound = 7
-    else:
-        upper_bound = amount + numeric_position[1]
-
-
-    binary_mat[numeric_position[0]][lower_bound:upper_bound+1] = 1 
-
-    binary_mat[numeric_position[0]][numeric_position[1]] = 0
+    lower_bound, upper_bound = find_bounds(position_row_column[1], amount)
+    binary_mat[position_row_column[0]][lower_bound:upper_bound+1] = 1 
+    binary_mat[position_row_column[0]][position_row_column[1]] = 0
     return binary_mat
 
-
-def vertical_squares(position, amount = 8):
-    numeric_position = algebraic_to_numeric(position)
+def vertical_squares(position_file_rank, amount = 8):
+    position_row_column = file_rank_to_row_col(position_file_rank)
     binary_mat = np.zeros((8,8))
+    lower_bound, upper_bound = find_bounds(position_row_column[0], amount)
+    
+    binary_mat[lower_bound:upper_bound+1][position_row_column[1]]= 1 
 
-    if  numeric_position[0]-amount < 0:
-        lower_bound = 0
-    else:
-        lower_bound = numeric_position[0] - amount 
-    if amount + numeric_position[0] > 7:
-        upper_bound = 7
-    else:
-        upper_bound = amount + numeric_position[0]
-
-
-    binary_mat[lower_bound:upper_bound+1][numeric_position[1]]= 1 
-
-    binary_mat[numeric_position[0]][numeric_position[1]] = 0
+    binary_mat[position_row_column[0]][position_row_column[1]] = 0
     return binary_mat
 
 def piece_id_to_color(piece_id):
