cb5b3dcd8447a7c5632af89c00c6909377d3b894
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-11 18:56:17 -0400

    minor refactoring

--- .VSCodeCounter/2023-07-11_18-54-52/diff.md
+++ .VSCodeCounter/2023-07-11_18-54-52/diff.md
@@ -1,19 +1,24 @@
 # Diff Summary
 
-Date : 2023-07-02 05:29:04
+Date : 2023-07-11 18:54:52
 
 Directory c:\\Users\\theor\\Desktop\\chess game
 
-Total : 0 files,  0 codes, 0 comments, 0 blanks, all 0 lines
+Total : 16 files,  -13 codes, 18 comments, 7 blanks, all 12 lines
 
 [Summary](results.md) / [Details](details.md) / Diff Summary / [Diff Details](diff-details.md)
 
 ## Languages
 | language | files | code | comment | blank | total |
 | :--- | ---: | ---: | ---: | ---: | ---: |
+| Python | 16 | -13 | 18 | 7 | 12 |
 
 ## Directories
 | path | files | code | comment | blank | total |
 | :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 16 | -13 | 18 | 7 | 12 |
+| . (Files) | 8 | -289 | 1 | -72 | -360 |
+| trash | 2 | -14 | 0 | 3 | -11 |
+| utilities | 6 | 290 | 17 | 76 | 383 |
 
 [Summary](results.md) / [Details](details.md) / Diff Summary / [Diff Details](diff-details.md)
--- trash/pieces.py
+++ trash/pieces.py
@@ -1,23 +1,11 @@
-import individual_pieces as ip
-class pieces:
-    def __init__(self, color):
-        if color == "black":
-            self.p = [ip.pawn("a7","black"),ip.pawn("b7","black"),ip.pawn("c7","black"),ip.pawn("d7","black"),ip.pawn("e7","black"),ip.pawn("f7","black"),ip.pawn("g7","black"),ip.pawn("h7","black")]
-            self.N = [ip.knight("b8","black"),ip.knight("g8","black")]
-            self.B = [ip.bishop("c8","black"),ip.bishop("f8","black")] 
-            self.R = [ip.rook("a8","black"),ip.rook("h8","black")]
-            self.Q = [ip.queen("d8","black")]
-            self.K = [ip.king("e8","black")]
-        else:
-            self.p = [ip.pawn("a2","white"),ip.pawn("b2","white"),ip.pawn("c2","white"),ip.pawn("d2","white"),ip.pawn("e2","white"),ip.pawn("f2","white"),ip.pawn("g2","white"),ip.pawn("h2","white")]
-            self.N = [ip.knight("b1","white"),ip.knight("g1","white")]
-            self.B = [ip.bishop("c1","white"),ip.bishop("f1","white")] 
-            self.R = [ip.rook("a1","white"),ip.rook("h1","white")]
-            self.Q = [ip.queen("d1","white")]
-            self.K = [ip.king("e1","white")]
-    def all_pieces(self):
-        return self.p + self.N + self.B + self.R + self.Q + self.K
-                       
- 
-aa = pieces("black")            
-print(type(aa.p))
+import Board as bd
+import Piece as pc
+
+
+class Pieces:
+    def __init__(self):
+        self.white_pieces = {}
+        self.black_pieces = {}
+    def set_to_initial_configuration(self):
+
+
