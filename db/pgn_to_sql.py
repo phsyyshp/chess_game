0c8170d931be67ce9c07dc6097d472a4cab1a4aa
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-14 01:38:19 -0400

    db stuff

--- db/pgn_to_sql.py
+++ db/pgn_to_sql.py
@@ -12,7 +12,8 @@ def remove_line_breaks(current_game: str):
 
 
 def extract_str_between_apost(field: str):
-    if "[" in field:
+    # print(field)
+    if '"' in field:
         return field.rsplit('"', 2)[1].replace("'", "") + "$"
     return field
 
@@ -79,9 +80,23 @@ def get_sql_command(game_info: str, move):
     game_info_list = game_info.split("$")
 
     values = "VALUES('" + "', '".join(game_info_list[:10]) + "', '" + move + "')" + ";"
-    # print(game_info)
+    # print(game_info_list)
     sql_command = (
         """INSERT INTO games(event, site, date, round, white, black, result, WhiteElo, BlackElo, ECO, moves)\n"""
+        + values
+    )
+    cursor.execute(sql_command)
+
+
+def get_sql_command_lichess(game_info: str, move):
+    game_info_list = game_info.split("$")
+
+    # print(game_info_list)
+    values = "VALUES('" + "', '".join(game_info_list[:15]) + "', '" + move + "')" + ";"
+    # print(game_info)
+    # print(move)
+    sql_command = (
+        """INSERT INTO lichess_games(event, site, white, black, result, utcdate, utctime, WhiteElo, BlackElo, whiteRatingDiff, blackRatingDiff, ECO, opening, timeControl, termination, moves)\n"""
         + values
     )
     cursor.execute(sql_command)
@@ -91,51 +106,63 @@ def pgn_to_sql(pgn_file):
     """
     Converts a pgn file to a sql file.
     """
-    with open(pgn_file, "r", encoding="utf8") as f:
-        listed_pgn = f.readlines()
-    arrayed_pgn = np.array(listed_pgn, dtype=str)
-    arrayed_pgn = remove_double_empty_lines(arrayed_pgn)
-    arrayed_pgn = add_blank_lines_if_missing(arrayed_pgn)
-    arrayed_pgn = add_blank_lines_if_missing_ab(arrayed_pgn)
-    arrayed_pgn = replace_game_seperator(arrayed_pgn)
+    with open(pgn_file, "r", encoding="latin-1") as f:
+        listed_pgn_all = f.readlines()
+    print(len(listed_pgn_all))
+    for i in range(500):
+        # partition listed pgn into 10 parts and create listed_pgn for i th partition
+        listed_pgn = listed_pgn_all[
+            i * len(listed_pgn_all) // 500 : (i + 1) * len(listed_pgn_all) // 500
+        ]
 
-    vec_tidy_up = np.vectorize(tidy_up)
-    arrayed_pgn = vec_tidy_up(arrayed_pgn)
-    string_pgn = "".join(arrayed_pgn)
-    arrayed_pgn = np.array(string_pgn.split("SEPERATOR"))
-    upper_bound_slice = (len(arrayed_pgn) // 2) * 2
-    game_info_array = arrayed_pgn[0 : upper_bound_slice - 10 : 2]
-    moves = arrayed_pgn[1 : upper_bound_slice - 9 : 2]
-    vec_get_sql_command = np.vectorize(get_sql_command)
+        arrayed_pgn = np.array(listed_pgn, dtype=str)
+        arrayed_pgn = remove_double_empty_lines(arrayed_pgn)
+        arrayed_pgn = add_blank_lines_if_missing(arrayed_pgn)
+        arrayed_pgn = add_blank_lines_if_missing_ab(arrayed_pgn)
+        arrayed_pgn = replace_game_seperator(arrayed_pgn)
 
-    vec_get_sql_command(game_info_array, moves)
+        vec_tidy_up = np.vectorize(tidy_up)
+        # print(arrayed_pgn)
+        arrayed_pgn = vec_tidy_up(arrayed_pgn)
+        string_pgn = "".join(arrayed_pgn)
+        string_pgn = string_pgn.rsplit("SEPERATOR", 1)[0]
+        arrayed_pgn = np.array(string_pgn.split("SEPERATOR"))
+        arrayed_pgn = np.append(arrayed_pgn, "SEPERATOR")
+        upper_bound_slice = (len(arrayed_pgn) // 2) * 2
+        game_info_array = arrayed_pgn[0 : upper_bound_slice - 10 : 2]
+        moves = arrayed_pgn[1 : upper_bound_slice - 9 : 2]
+        vec_get_sql_command = np.vectorize(get_sql_command_lichess)
+
+        # print(game_info_array)
+        # print(moves)
+        vec_get_sql_command(game_info_array, moves)
 
 
-directory = "pgn_files"
-i = 0
-for file_name in os.listdir(directory):
-    pgn_file = os.path.join(directory, file_name)
-    print("Conversion of {file_name:} is being done".format(file_name=file_name))
-    i += 1
-    pgn_to_sql(pgn_file)
-    total_number_of_filed = len(os.listdir(directory))
-    percentage = i / total_number_of_filed * 100
-    print("Conversion of {file_name:} is finished".format(file_name=file_name))
-    print(
-        "{percentage:3.1f}% ofSELECT COUNT(*) FROM games; all files is done.".format(
-            percentage=percentage
-        )
-    )
-    print(
-        "{i}|{total_number_of_filed}".format(
-            i=i, total_number_of_filed=total_number_of_filed
-        )
-    )
-    print("------------------------------------------------------------")
+# directory = "pgn_files"
+# i = 0
+# for file_name in os.listdir(directory):
+#     pgn_file = os.path.join(directory, file_name)
+#     print("Conversion of {file_name:} is being done".format(file_name=file_name))
+#     i += 1
+#     pgn_to_sql(pgn_file)
+#     total_number_of_filed = len(os.listdir(directory))
+#     percentage = i / total_number_of_filed * 100
+#     print("Conversion of {file_name:} is finished".format(file_name=file_name))
+#     print(
+#         "{percentage:3.1f}% ofSELECT COUNT(*) FROM games; all files is done.".format(
+#             percentage=percentage
+#         )
+#     )
+#     print(
+#         "{i}|{total_number_of_filed}".format(
+#             i=i, total_number_of_filed=total_number_of_filed
+#         )
+#     )
+#     print("------------------------------------------------------------")
 
 
 # pgn_to_sql(pgn_file="pgn_files/KIDOther56.pgn")
 
-
+pgn_to_sql("pgn_files/lichess.pgn")
 sqliteConnection.commit()
 sqliteConnection.close()
