1cca04dc6bd93a0c75fbf51b2b8c7fee889df509
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-14 06:58:01 -0400

    major refactoring of parser

--- db/pgn_to_sql.py
+++ db/pgn_to_sql.py
@@ -1,106 +1,125 @@
 import sqlite3
 import numpy as np
+from dbutilities import *
 import os
+
+
 class SQLjobs:
-    def __init__(self):
+    def __init__(self, database_name):
+        self.database_name = database_name
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.database_name)
+        self.cursor = self.connection.cursor()
+
+    def close_commit(self):
+        self.connection.commit()
+        self.connection.close()
+
+
+class PgnFile:
+    def __init__(self, pgn_file) -> None:
+        self.pgn_file = pgn_file
+
+    def __enter__(self):
+        self.file = open(self.pgn_file, "r", encoding="latin-1")
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+    def read_lines(self, nol):
+        return self.file.readlines(nol)
+
+    def get_file(self):
         pass
-    def functoimp(self):
-        sqliteConnection = sqlite3.connect("master_games.db")
-        cursor = sqliteConnection.cursor()
-    def func2toimp(self):
-        sqliteConnection.commit()
-        sqliteConnection.close()
-        
+
+    # def pgn_stuff(self):
+    #     directory = "pgn_files"
+    #     i = 0
+    #     for file_name in os.listdir(directory):
+    #         pgn_file = os.path.join(directory, file_name)
+    #         print(
+    #             "Conversion of {file_name:} is being done".format(file_name=file_name)
+    #         )
+    #         i += 1
+    #         pgn_to_sql(pgn_file)
+    #         total_number_of_filed = len(os.listdir(directory))
+    #         percentage = i / total_number_of_filed * 100
+    #         print("Conversion of {file_name:} is finished".format(file_name=file_name))
+    #         print(
+    #             "{percentage:3.1f}% ofSELECT COUNT(*) FROM games; all files is done.".format(
+    #                 percentage=percentage
+    #             )
+    #         )
+    #         print(
+    #             "{i}|{total_number_of_filed}".format(
+    #                 i=i, total_number_of_filed=total_number_of_filed
+    #             )
+    #         )
+    #         print("------------------------------------------------------------")
+
 
 class PgnParser:
     def __init__(self, pgn_lines: np.ndarray) -> None:
         self.pgn_lines = pgn_lines
 
     def _remove_double_empty_lines(self):
-        empty_lines_mask = (self.pgn_lines == "\n")
-        empty_lines_mask_left_shifted = np.append(empty_lines_mask[1:], False)
-        double_empty_lines_mask = empty_lines_mask_left_shifted * empty_lines_mask
-        pgn_lines_no_double_blank_lines= pgn_lines[0 == double_empty_lines_mask] 
-        self.pgn_lines = pgn_lines_no_double_blank_lines
+        blank_lines_mask = self.pgn_lines == "\n"
+        # print(blank_lines_mask)
+        blank_lines_mask_left_shifted = shift(blank_lines_mask, -1)
+        double_blank_lines_mask = blank_lines_mask_left_shifted * blank_lines_mask
+        self.pgn_lines = self.pgn_lines[0 == double_blank_lines_mask]
+
+    def _add_missing_empty_lines(self, direction):
+        game_info_lines_mask = vec_does_contain_bracket(self.pgn_lines)
+        slided_game_info_lines_mask = shift(game_info_lines_mask, direction)
+        last_game_info_lines_mask = game_info_lines_mask * (
+            False == slided_game_info_lines_mask
+        )
+        slided_last_game_info_lines_mask = shift(last_game_info_lines_mask, -direction)
+        missing_line_mask = (self.pgn_lines != "\n") * slided_last_game_info_lines_mask
+        slided_missing_line_mask = shift(missing_line_mask, direction)
+        indices = np.nonzero(slided_missing_line_mask == True)[0].tolist()
+        if len(indices) != 0:
+            self.pgn_lines = np.insert(self.pgn_lines, indices, "\n")
+
+    def _rtrim(self):
+        string_pgn = "".join(self.pgn_lines)
+        string_pgn = string_pgn.rsplit("SEPERATOR", 1)[0]
+        self.pgn_lines = np.array(string_pgn.split("SEPERATOR"))
+        self.pgn_lines = np.append(self.pgn_lines, "SEPERATOR")
+
+    def _replace_blank_lines_with_seperator(self):
+        blank_lines_mask = self.pgn_lines == "\n"
+        self.pgn_lines[blank_lines_mask] = "SEPERATOR"
+
+    def _format_each_line(self):
+        tidying_lambda = lambda item: extract_str_between_apost(
+            remove_line_breaks(item)
+        )
+        vectorized_format_each_line = np.vectorize(tidying_lambda)
+        self.pgn_lines = vectorized_format_each_line(self.pgn_lines)
 
     def parse(self):
         self._remove_double_empty_lines()
-        self._add_blank_lines_if_missing()
-        self._add_blank_lines_if_missing_ab()
-        self._replace_game_seperator()
-        self._vec_tidy_up()
-            vec_tidy_up = np.vectorize(tidy_up)
-            # print(arrayed_pgn)
-            string_pgn = "".join(arrayed_pgn)
-            string_pgn = string_pgn.rsplit("SEPERATOR", 1)[0]
-            arrayed_pgn = np.array(string_pgn.split("SEPERATOR"))
-            arrayed_pgn = np.append(arrayed_pgn, "SEPERATOR")
-            upper_bound_slice = (len(arrayed_pgn) // 2) * 2
-            game_info_array = arrayed_pgn[0 : upper_bound_slice - 10 : 2]
-            moves = arrayed_pgn[1 : upper_bound_slice - 9 : 2]
-            vec_get_sql_command = np.vectorize(get_sql_command_lichess)
-vec_does_contain_bracket = np.vectorize(is_contain_bracket)
-    def remove_line_breaks(current_game: str):
-        current_game = current_game.replace("\n", " ")
-        return current_game
-
-    def is_contain_bracket(field: str):
-        return "[" in field
-
-    def extract_str_between_apost(field: str):
-        # print(field)
-        if '"' in field:
-            return field.rsplit('"', 2)[1].replace("'", "") + "$"
-        return field
-
-    def tidy_up(item):
-        item = remove_line_breaks(item)
-        return extract_str_between_apost(item)
-
-    
-
-    def replace_game_seperator(arrayed_pgn):
-        empty_lines_mask = arrayed_pgn == "\n"
-        arrayed_pgn[empty_lines_mask] = "SEPERATOR"
-        return arrayed_pgn
-
-    def add_blank_lines_if_missing(arrayed_pgn):
-        bracket_mask = vec_does_contain_bracket(arrayed_pgn)
-        slided_bracket_mask = np.append(bracket_mask[1:], False)
-        last_bracket_mask = bracket_mask * (False == slided_bracket_mask)
-        # add False in front of array
-        slided_last_bracket_mask = np.append(False, last_bracket_mask[:-1])
-        missing_line_mask = (arrayed_pgn != "\n") * slided_last_bracket_mask
-
-        slided_missing_line_mask = np.append(missing_line_mask[1:], False)
-        indices = np.nonzero(slided_missing_line_mask == True)[0].tolist()
-        if len(indices) == 0:
-            return arrayed_pgn
-        return np.insert(arrayed_pgn, indices, "\n")
-        # arrayed_pgn[slided_missing_line_mask ] = "\n"
-
-    def add_blank_lines_if_missing_ab(arrayed_pgn):
-        bracket_mask = vec_does_contain_bracket(arrayed_pgn)
-        slided_bracket_mask = np.append(False, bracket_mask[:-1])
-        last_bracket_mask = bracket_mask * (False == slided_bracket_mask)
-        # add False in front of array
-        slided_last_bracket_mask = np.append(last_bracket_mask[1:], False)
-        missing_line_mask = (arrayed_pgn != "\n") * slided_last_bracket_mask
-
-        slided_missing_line_mask = np.append(False, missing_line_mask[:-1])
-        indices = np.nonzero(slided_missing_line_mask == True)[0].tolist()
-        # print(indices)
-        if len(indices) == 0:
-            return arrayed_pgn
-        return np.insert(arrayed_pgn, indices, "\n")
-        # arrayed_pgn[slided_missing_line_mask ] = "\n"
+        self._add_missing_empty_lines(-1)
+        self._add_missing_empty_lines(1)
+        self._replace_blank_lines_with_seperator()
+        self._format_each_line()
+        # self._ltrim()
+        self._rtrim()
+        return self.pgn_lines
 
 
 class PgnToSQL:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cursor: sqlite3.Cursor, table_name) -> None:
+        self.cursor = cursor
+        self.table_name = table_name
 
-    def get_sql_command(game_info: str, move):
+    def generate_sql_command_reg(self, game_info: str, move):
         game_info_list = game_info.split("$")
 
         values = (
@@ -111,94 +130,44 @@ class PgnToSQL:
             """INSERT INTO games(event, site, date, round, white, black, result, WhiteElo, BlackElo, ECO, moves)\n"""
             + values
         )
-        cursor.execute(sql_command)
+        self.cursor.execute(sql_command)
 
-    def get_sql_command_lichess(game_info: str, move):
+    def generate_sql_command(self, game_info: str, move):
         game_info_list = game_info.split("$")
-
-        # print(game_info_list)
         values = (
             "VALUES('" + "', '".join(game_info_list[:15]) + "', '" + move + "')" + ";"
         )
-        # print(game_info)
-        # print(move)
         sql_command = (
-            """INSERT INTO lichess_games(event, site, white, black, result, utcdate, utctime, WhiteElo, BlackElo, whiteRatingDiff, blackRatingDiff, ECO, opening, timeControl, termination, moves)\n"""
+            """INSERT INTO {table_name:}(event, site, white, black, result, utcdate, utctime, WhiteElo, BlackElo, whiteRatingDiff, blackRatingDiff, ECO, opening, timeControl, termination, moves)\n""".format(
+                table_name=self.table_name
+            )
             + values
         )
-        cursor.execute(sql_command)
+        self.cursor.execute(sql_command)
 
-    def pgn_to_sql(pgn_file):
-        """
-        Converts a pgn file to a sql file.
-        """
-        with open(pgn_file, "r", encoding="latin-1") as f:
-            pgn_lines_all = f.readlines()
-        print(len(pgn_lines_all))
-        for i in range(500):
-            # partition listed pgn into 10 parts and create pgn_lines for i th partition
-            pgn_lines = pgn_lines_all[
-                i * len(pgn_lines_all) // 500 : (i + 1) * len(pgn_lines_all) // 500
-            ]
+    def pgn_to_sql(self, pgn_file):
+        with PgnFile(pgn_file) as pgn:
+            pgn_lines = pgn.read_lines(100000)
 
-            arrayed_pgn = np.array(pgn_lines, dtype=str)
-            arrayed_pgn = remove_double_empty_lines(arrayed_pgn)
-            arrayed_pgn = add_blank_lines_if_missing(arrayed_pgn)
-            arrayed_pgn = add_blank_lines_if_missing_ab(arrayed_pgn)
-            arrayed_pgn = replace_game_seperator(arrayed_pgn)
+        pgn_lines = np.array(pgn_lines)
+        pgn_lines = PgnParser(pgn_lines).parse()
+        upper_bound_slice = (len(pgn_lines) // 2) * 2
+        game_info_array = pgn_lines[0 : upper_bound_slice - 10 : 2]
+        moves = pgn_lines[1 : upper_bound_slice - 9 : 2]
+        vec_generate_sql_command = np.vectorize(self.generate_sql_command)
 
-            vec_tidy_up = np.vectorize(tidy_up)
-            # print(arrayed_pgn)
-            arrayed_pgn = vec_tidy_up(arrayed_pgn)
-            string_pgn = "".join(arrayed_pgn)
-            string_pgn = string_pgn.rsplit("SEPERATOR", 1)[0]
-            arrayed_pgn = np.array(string_pgn.split("SEPERATOR"))
-            arrayed_pgn = np.append(arrayed_pgn, "SEPERATOR")
-            upper_bound_slice = (len(arrayed_pgn) // 2) * 2
-            game_info_array = arrayed_pgn[0 : upper_bound_slice - 10 : 2]
-            moves = arrayed_pgn[1 : upper_bound_slice - 9 : 2]
-            vec_get_sql_command = np.vectorize(get_sql_command_lichess)
-
-            # print(game_info_array)
-            # print(moves)
-            vec_get_sql_command(game_info_array, moves)
-
-
-class PgnFile:
-    def __init__(self) -> None:
-        pass
-
-    def get_file(self):
-        pass
-    def pgn_stuff(self):
-        directory = "pgn_files"
-        i = 0
-        for file_name in os.listdir(directory):
-            pgn_file = os.path.join(directory, file_name)
-            print("Conversion of {file_name:} is being done".format(file_name=file_name))
-            i += 1
-            pgn_to_sql(pgn_file)
-            total_number_of_filed = len(os.listdir(directory))
-            percentage = i / total_number_of_filed * 100
-            print("Conversion of {file_name:} is finished".format(file_name=file_name))
-            print(
-                "{percentage:3.1f}% ofSELECT COUNT(*) FROM games; all files is done.".format(
-                    percentage=percentage
-                )
-            )
-            print(
-                "{i}|{total_number_of_filed}".format(
-                    i=i, total_number_of_filed=total_number_of_filed
-                )
-            )
-            print("------------------------------------------------------------")
-
-
-
-
+        # print(game_info_array)
+        # print(moves)
+        vec_generate_sql_command(game_info_array, moves)
 
 
 # pgn_to_sql(pgn_file="pgn_files/KIDOther56.pgn")
 def main():
-    pgn_to_sql("pgn_files/lichess.pgn")
+    sqlhandler = SQLjobs("master_games.db")
+    sqlhandler.connect()
+    PgnToSQL(sqlhandler.cursor, "lichess_games").pgn_to_sql("pgn_files/lichess.pgn")
+    # pgn_to_sql("pgn_files/lichess.pgn")
+    sqlhandler.close_commit()
+
+
 main()
