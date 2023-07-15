b6d6b9e86fff6363ed2174903291f8803d0bc63c
phsyyshp <theoriginal169@gmail.com>
2023-07-15 12:29:14 -0400

    working db uploader numpy

--- db/dbutilities.py
+++ db/dbutilities.py
@@ -12,7 +12,7 @@ def is_contain_bracket(field: str):
 
 def extract_str_between_apost(field: str):
     if '"' in field:
-        return field.rsplit('"', 2)[1].replace("'", "") + "$"
+        return ">".join(field.rsplit('"', 2)[:2]).replace("'", "") + "$"
     return field
 
 
@@ -20,6 +20,39 @@ def shift(array, direction):
     if direction == -1:
         return np.append(array[1:], False)
     return np.append(False, array[:-1])
+
+
+def get_first_occurence_index(array, element):
+    return np.where(array == element)[0][0]
+
+
+def get_last_occurence_index(array, element):
+    return np.where(array == element)[0][-1]
+
+
+def get_next_to_last_occurence_index(array, element):
+    return np.where(array == element)[0][-2]
+
+
+def get_next_to_first_occurence_index(array, element):
+    return np.where(array == element)[0][1]
+
+
+def get_field_tag(item: str):
+    out = item.split(">")[0]
+    return out[1:].strip()
+
+
+def get_field_value(item: str):
+    out = item.split(">")[1]
+    return out
+
+
+def game_info_to_dic(game_info: list):
+    # print(game_info)
+    field_tag_list = list(map(get_field_tag, game_info))
+    field_value_list = list(map(get_field_value, game_info))
+    return dict(zip(field_tag_list, field_value_list))
 
 
 vec_does_contain_bracket = np.vectorize(is_contain_bracket)
