74ed9a6a37b1c57232ac22913570311936017888
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-19 15:40:08 -0400

    many performance improvements

--- GameState.py
+++ GameState.py
@@ -3,7 +3,7 @@ import numpy as np
 import Piece as pc
 import Board as bd
 import Move as mv
-
+import cProfile
 
 class GameState:
     def __init__(self, board: bd.Board):
@@ -15,19 +15,19 @@ class GameState:
     ):
         moves_mask = piece_object.squares_in_range_mask()
         moves_row_column = mask_to_row_column(moves_mask)
-        legal_moves = np.array(list(map(
+        legal_moves_mask = np.array(list(map(
             lambda x: mv.Move(piece_object.position_row_column, x, self.board).is_legal(), moves_row_column
         )))
-        return moves_row_column[legal_moves]
+        return moves_row_column[legal_moves_mask]
 
     def get_all_legal_moves(self, color):
         positions_of_pieces = self.board.get_all_same_color_piece_positions(color)
-        legal_moves = []
-        for position_of_piece in positions_of_pieces:
-            legal_moves += self.get_legal_moves(
-                self.board.get_piece_object_from_position(position_of_piece)
-            )
-        return legal_moves
+        legal_moves =  (list(map(
+            lambda x: self.get_legal_moves(self.board.get_piece_object_from_position(x)), positions_of_pieces
+        )))
+        #remove empty arrays
+        # legal_moves = list(filter(lambda x: len(x) > 0, legal_moves))
+        return  positions_of_pieces.tolist(), legal_moves
 
     def can_king_be_saved(self):
         # TODO there are jobs to do here.
@@ -44,3 +44,23 @@ class GameState:
 
     def is_stale_mate(self):
         return False
+
+
+bb = bd.Board()
+bb.set_board_to_initial_configuration()
+gs = GameState(bb)
+#benchmark
+import timeit 
+
+# print(gs.get_all_legal_moves("white")[0])
+# print(gs.get_all_legal_moves("white")[1])
+ 
+# print(len(gs.get_all_legal_moves("white")[1]))
+# print(len(gs.get_all_legal_moves("white")[0]))
+def dd():
+    gs.get_all_legal_moves("white")
+cProfile.run("dd()")
+def a():
+    1
+execution_time = timeit.timeit( a, number=1_000_000_000)
+print(execution_time)
---
+++ movegenerator.py
@@ -0,0 +1,74 @@
+import GameState as gs
+import Board as bd
+import Visual as vs
+import os
+
+class MoveGenerator:
+    def __init__(self, board: bd.Board):
+        self.board = board
+        
+
+    def play_multiple_moves(self, old_moves, new_moves):
+        boards = []
+        for old_move, new_move in zip(old_moves, new_moves):
+            for nm in new_move:
+                if len(nm.tolist()) == 0:
+                    continue
+                board_copy = self.board.copy()
+                board_copy.move(old_move,nm) 
+                board_copy.change_turn()
+            # print( board_copy.board_matrix)
+                boards.append(board_copy)
+        return boards
+    
+    def play_all_legal_moves(self, color):
+        old_moves, new_moves = gs.GameState(self.board).get_all_legal_moves(color)
+        return self.play_multiple_moves(old_moves, new_moves)
+    
+    def generate_all_moves_in_depth(self, depth):
+        boards = self.play_all_legal_moves("white")
+        current_boards = boards.copy()
+        tempbds = []
+        color_dic =["black", "white"]
+        i=0
+        while i < depth:
+            print(len(current_boards))
+
+            for board_item in current_boards:
+                new_boards = MoveGenerator(board_item).play_all_legal_moves(color_dic[i%2]) 
+                boards+=new_boards
+                tempbds+=new_boards
+            i+=1
+            current_boards = tempbds.copy()
+            tempbds = []
+             
+        return boards
+    # def get_moves_tree(self, depth)
+    #     counter = 0
+    #     move_tree = [gs.GameState(self.board).get_all_legal_moves("white")]
+    #     for i in range(depth):
+    #         for level 
+    #            boards = self.play_multiple_moves(move_tree[i])
+        
+    #         all_legal_moves = gs.GameState(self.board).get_all_legal_moves(color)
+            
+bb = bd.Board()
+bb.set_board_to_initial_configuration()
+# bb.set_board_to_fen_configuration("4rbk1/3p1p2/1p2p2p/p3B1pQ/3PB3/4P1KP/5PP1/4q3")
+print(vs.Visual("white", bb).show())
+mg = MoveGenerator(bb)
+board_tree = mg.generate_all_moves_in_depth(1)
+# with open("board_tree.txt", "w", encoding="utf8") as f:
+#     for board in board_tree:
+#         f.write(vs.Visual("white", board).show())
+#         f.write("\n")
+import time
+# for board in board_tree:
+
+#     os.system('cls||clear')
+#     # print(board)
+     
+#     b = vs.Visual("white", board).show() 
+#     print(b)
+#     # time.sleep(1)
+print(len(board_tree))
