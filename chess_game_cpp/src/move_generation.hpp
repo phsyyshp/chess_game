e9105c403f2ac6498d00fe0fd04d6d98aba9fa71
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-21 22:29:49 -0400

    pawn pushes

--- chess_game_cpp/src/move_generation.hpp
+++ chess_game_cpp/src/move_generation.hpp
@@ -4,9 +4,6 @@
 
 class MoveGeneration {
 public:
-  // TODO add attributes
-  //   void generate_all_legal_moves(Position position, bool color);
-  //   void generate_legal_moves_of_specific_piece(Position position);
   std::vector<uint64_t> factor_mask(uint64_t mask);
   uint64_t generate_single_pawn_pushes(Position position, int color);
   uint64_t generate_double_pawn_pushes(Position position, int color);
