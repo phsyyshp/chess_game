18ee703f127ca45536691dab9cf7fad33c27e532
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-07-29 04:41:04 -0400

    fixed bug in maks gen of rook

--- chess_game_cpp/src/mask_generation.cpp
+++ chess_game_cpp/src/mask_generation.cpp
@@ -16,20 +16,20 @@ void print_board(uint64_t pieces) {
     cout << "\n";
   }
 }
-uint64_t selectorf(uint64_t input, int selector) {
-  if (selector == 1) {
-    return input;
-  } else {
-    return 0b1ULL >> 1;
-  }
-}
 uint64_t remove_bit(uint64_t input, int bit) {
   return input & ~(0b1ULL << bit);
 }
-uint64_t generate_bishop_mask(uint64_t position, int slope, int amount) {
+vector<int> position_to_row_col(uint64_t position) {
+
   int linear_position = __builtin_ctzll(position);
   int column = linear_position % 8;
-  int row = floor(linear_position / 8); // - (row * 8);
+  int row = floor(linear_position / 8);
+  return {row, column};
+}
+uint64_t generate_bishop_mask(uint64_t position, int slope, int amount) {
+  vector<int> row_column = position_to_row_col(position);
+  int row = row_column[0];
+  int column = row_column[1];
   int offset = row - slope * column;
   uint64_t diag_mask = 0ULL;
   for (int i = -amount + column; i < (amount + column); i++) {
@@ -41,8 +41,6 @@ uint64_t generate_bishop_mask(uint64_t position, int slope, int amount) {
   }
   return diag_mask;
 }
-
-// vector of length 64 uint64_t is output
 vector<uint64_t> generate_bishop_masks() {
   vector<uint64_t> masks;
 
@@ -53,9 +51,9 @@ vector<uint64_t> generate_bishop_masks() {
   return masks;
 }
 uint64_t generate_rook_mask(uint64_t position, int amount) {
-  int linear_position = __builtin_ctzll(position);
-  int column = linear_position % 8;
-  int row = floor(linear_position / 8);
+  vector<int> row_column = position_to_row_col(position);
+  int row = row_column[0];
+  int column = row_column[1];
   uint64_t mask = 0ULL;
   for (int i = 1; i < 7; i++) {
     mask |= 0b1ULL << (row * 8 + i);
@@ -82,11 +80,11 @@ vector<uint64_t> generate_queen_masks() {
   return masks;
 }
 
-vector<uint64_t> generate_relevant_occupancy_masks(uint64_t position) {
+vector<uint64_t> generate_relevant_rook_occupancy_masks(uint64_t position) {
   MoveGeneration move_generator;
   int linear_position = __builtin_ctzll(position);
   int column = linear_position % 8;
-  int row = linear_position / 8;
+  int row = floor(linear_position / 8); // - (row * 8);
   uint64_t mask = 0ULL;
   uint64_t attack_mask = generate_rook_mask(position, 8);
   attack_mask = remove_bit(attack_mask, linear_position);
@@ -107,25 +105,34 @@ vector<uint64_t> generate_relevant_occupancy_masks(uint64_t position) {
   return out;
 }
 
-uint64_t generate_attack_mask(uint64_t occupancy_mask, uint64_t position) {
+uint64_t generate_rook_attack_mask(uint64_t occupancy_mask, uint64_t position) {
   MoveGeneration move_generator;
   int linear_position = __builtin_ctzll(position);
   int column = linear_position % 8;
   int row = floor(linear_position / 8);
   int i = 0;
   uint64_t attack_mask = 0ULL;
-  while (((occupancy_mask & (0b1uLL << (row * 8 + column + i))) == 0) &
-         ((column + i) <= 7)) {
-    attack_mask |= 0b1uLL << (row * 8 + column + i);
-    i++;
+  while (((column + i) <= 7)) {
+    if ((occupancy_mask & (0b1uLL << (row * 8 + column + i))) == 0) {
+      attack_mask |= 0b1uLL << (row * 8 + column + i);
+      i++;
+    } else {
+      attack_mask |= (0b1uLL << (row * 8 + column + i));
+      break;
+    }
   }
 
   i = 0;
-  while (((occupancy_mask & (0b1uLL << (row * 8 + column - i))) == 0) &
-         ((column - i) >= 0)) {
-    attack_mask |= 0b1uLL << (row * 8 + column - i);
-    i++;
+  while (((column - i) >= 0)) {
+    if ((occupancy_mask & (0b1uLL << (row * 8 + column - i))) == 0) {
+      attack_mask |= 0b1uLL << (row * 8 + column - i);
+      i++;
+    } else {
+      attack_mask |= (0b1uLL << (row * 8 + column - i));
+      break;
+    }
   }
+
   i = 0;
   while (((occupancy_mask & (0b1uLL << (row * 8 + column + i * 8))) == 0) &
          ((row + i) <= 7)) {
@@ -141,12 +148,12 @@ uint64_t generate_attack_mask(uint64_t occupancy_mask, uint64_t position) {
   attack_mask = remove_bit(attack_mask, linear_position);
   return attack_mask;
 }
-vector<uint64_t> generate_attack_masks(uint64_t position) {
+vector<uint64_t> generate_rook_attack_masks(uint64_t position) {
   vector<uint64_t> relevant_occupancy_masks =
-      generate_relevant_occupancy_masks(position);
+      generate_relevant_rook_occupancy_masks(position);
   vector<uint64_t> attack_masks;
   for (auto mask : relevant_occupancy_masks) {
-    attack_masks.push_back(generate_attack_mask(mask, position));
+    attack_masks.push_back(generate_rook_attack_mask(mask, position));
   }
   return attack_masks;
 }
@@ -162,19 +169,25 @@ int main() {
   vector<uint64_t> out2 = generate_bishop_masks();
   vector<uint64_t> out3 = generate_rook_masks();
   vector<uint64_t> out4 = generate_queen_masks();
-  vector<uint64_t> out5 = generate_relevant_occupancy_masks(0b1ULL << 21);
-  vector<uint64_t> out6 = generate_attack_masks(0b1ULL << 21);
+  vector<uint64_t> out5 = generate_relevant_rook_occupancy_masks(0b1ULL << 21);
+  vector<uint64_t> out6 = generate_rook_attack_masks(0b1ULL << 21);
   cout << "inmasks" << endl;
   cout << out5.size() << endl;
   // cout << generate_rook_mask(0b1ULL, 8) << endl;
   print_board(generate_rook_mask(0b1ULL << 21, 21));
   print_board(out5[10]);
   cout << "attack_mask" << endl;
-  print_board(generate_attack_mask(out5[10], 0b1ULL << 21));
+  print_board(generate_rook_attack_mask(out5[10], 0b1ULL << 21));
+  int i = 0;
   for (auto mask : out6) {
+    cout << "relevant rook occ" << endl;
+    print_board(out5[i]);
+    cout << "attack mask" << endl;
+
     print_board(mask);
     // cout << mask << endl;
     cout << "\n";
+    i++;
   }
   // for (auto mask : out2) {
   //   print_board(mask);
