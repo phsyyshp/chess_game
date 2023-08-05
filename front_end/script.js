ddb66ff2f329a8f2df0a65f91c160b36d8526892
phsyyshp <27218386+phsyyshp@users.noreply.github.com>
2023-08-05 05:49:10 -0400

    .

--- front_end/script.js
+++ front_end/script.js
@@ -1,1 +1,11 @@
-console.log("lalala");
+let openings = document.getElementById("openings");
+let openingsDropDown = document.getElementById("openingsDropDown");
+openingsDropDown.style.display = "none";
+function showOpenings() {
+  openingsDropDown.style.display = "block";
+}
+function hideOpenings() {
+  openingsDropDown.style.display = "none";
+}
+openings.addEventListener("mouseover", showOpenings);
+openings.addEventListener("mouseleave", hideOpenings);
