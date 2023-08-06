69a85e14d7633337359d36ecb759ddce4657127e
phsyyshp <theoriginal169@gmail.com>
2023-08-06 02:56:17 -0400

    added board

--- front_end/script.js
+++ front_end/script.js
@@ -1,11 +1,14 @@
-let openings = document.getElementById("openings");
-let openingsDropDown = document.getElementById("openingsDropDown");
-openingsDropDown.style.display = "none";
-function showOpenings() {
-  openingsDropDown.style.display = "block";
+let chessBoard = document.getElementById("chess_board");
+const rows = [];
+for (let i = 0; i < 8; i++) {
+  rows[i] = chessBoard.insertRow(i);
+  for (let j = 0; j < 8; j++) {
+    let cell = rows[i].insertCell(j);
+    cell.innerHTML = "  a ";
+    if ((i + j) % 2 === 0) {
+      cell.className = "whiteCells";
+    } else {
+      cell.className = "blackCells";
+    }
+  }
 }
-function hideOpenings() {
-  openingsDropDown.style.display = "none";
-}
-openings.addEventListener("mouseover", showOpenings);
-openings.addEventListener("mouseleave", hideOpenings);
