182b62fca72da1a2925a56152339b2f5d1c48b03
“physsyhp” <theoriginal169@gmail.com>
2023-12-31 20:33:04 -0500

    a lot of changes

--- chess_game_cpp/src/loader.cpp
+++ chess_game_cpp/src/loader.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <vector>
 std::vector<uint64_t> line_to_nums_vec(const std::string &line) {
-
   std::vector<uint64_t> out;
   std::string number;
   for (auto c : line) {
@@ -16,9 +15,8 @@ std::vector<uint64_t> line_to_nums_vec(const std::string &line) {
   }
   return out;
 }
-
 std::vector<uint64_t> read_magic_numbers(std::string piece) {
-  std::string file_name = piece + "_magic_numbers.txt";
+  std::string file_name = "mask_cache/" + piece + "_magic_numbers.txt";
   std::vector<uint64_t> out;
   std::string temp;
   std::fstream in(file_name);
@@ -33,8 +31,7 @@ std::vector<uint64_t> read_magic_numbers(std::string piece) {
   return out;
 }
 std::vector<std::vector<uint64_t>> read_look_up_tables(std::string piece) {
-
-  std::string file_name = piece + "_look_up_tables.txt";
+  std::string file_name = "mask_cache/" + piece + "_look_up_tables.txt";
   std::vector<std::vector<uint64_t>> look_up_tables;
   std::string line;
   std::fstream in(file_name);
@@ -50,4 +47,38 @@ std::vector<std::vector<uint64_t>> read_look_up_tables(std::string piece) {
   }
   in.close();
   return look_up_tables;
+}
+std::vector<int> position_to_row_col(uint64_t position) {
+
+  int linear_position = __builtin_ctzll(position);
+  int column = linear_position % 8;
+  int row = (linear_position / 8);
+  return {row, column};
+}
+int rook_relevant_bits(uint64_t position) {
+  std::vector<int> row_col_vec = position_to_row_col(position);
+  int row = row_col_vec[0];
+  int column = row_col_vec[1];
+
+  int relevant_bits;
+  bool is_at_sides = (column == 0 || column == 7);
+  bool is_at_topbot = (row == 0 || row == 7);
+  if ((is_at_topbot && !is_at_sides) || (!is_at_topbot && is_at_sides)) {
+    relevant_bits = 11;
+  } else if (!is_at_topbot && !is_at_sides) {
+    relevant_bits = 10;
+  } else {
+    relevant_bits = 12;
+  }
+  return relevant_bits;
+}
+uint64_t generate_magic_index(uint64_t bitboard, uint64_t magic_number,
+                              int shiftBits) {
+  return (bitboard * magic_number) >> shiftBits;
+}
+uint64_t get_rook_attack_mask(u_int64_t position, u_int64_t bitboard) {
+  int bits = rook_relevant_bits(position);
+  int shiftBits = 64 - bits;
+  uint64_t magic_index =
+      generate_magic_index(bitboard, magic_number, shiftBits);
 }
