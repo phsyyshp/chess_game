b6a43b24f60cdee4a0b9d559b65161b62414f747
“physsyhp” <theoriginal169@gmail.com>
2024-01-12 08:13:29 -0500

    bunch of minor bugs

--- chess_game_cpp/src/move_generation.hpp
+++ chess_game_cpp/src/move_generation.hpp
@@ -1,14 +1,23 @@
 #include "Move.hpp"
 #include "position.hpp"
 class MoveGeneration {
+
 public:
+  MoveGeneration() = default;
   std::vector<uint64_t> factor_mask(uint64_t mask);
   void generateSinglePawnPushes(Position position, const color &coloIn);
   void generateDoublePawnPushes(Position position, const color &coloIn);
-  void generatePawnCaptures(Position position, const color &colorIn);
+  void generateLeftPawnCaptures(Position position, const color &colorIn);
+  void generateRightPawnCaptures(Position position, const color &colorIn);
   void generateKnightMoves(Position position, const color &coloIn);
   void generateBishopMoves(Position position, const color &colorIn);
   void generateRookMoves(Position position, const color &colorIn);
   void generateQueenMoves(Position position, const color &colorIn);
   void generateKingMoves(Position position, const color &colorIn);
+  void generateAllMoves(Position position, const color &colorIn);
+  // getters
+  std::vector<Move> getMoves() const;
+
+private:
+  std::vector<Move> moveList;
 };
