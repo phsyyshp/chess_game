b400d1304b3c362627e343bcd7c015e7af9f5514
“physsyhp” <theoriginal169@gmail.com>
2024-01-13 08:46:36 -0500

    add several depths to perft test

--- chess_game_cpp/src/Move.cpp
+++ chess_game_cpp/src/Move.cpp
@@ -3,4 +3,4 @@ int Move::getFrom() const { return moveNum & 0x3f; }
 int Move::getTo() const { return (moveNum >> 6) & 0x3f; }
 int Move::getColor() const { return (moveNum >> 12) & 0x3f; }
 int Move::getPiece() const { return (moveNum >> 18) & 0x3f; }
-bool Move::checkIsCapture() const { return (moveNum >> 21) == 1; }
+bool Move::checkIsCapture() const { return (moveNum >> 24) == 1; }
---
+++ chess_game_cpp/src/moveTest.cpp
@@ -0,0 +1,4 @@
+#include "Move.hpp"
+int main(
+
+);
