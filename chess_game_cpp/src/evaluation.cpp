8bca44e4f4a99da8493a1f06fd5905f2313a1459
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-01-21 09:58:47 -0500

    a bug in eval is fixed

--- chess_game_cpp/src/evaluation.cpp
+++ chess_game_cpp/src/evaluation.cpp
@@ -29,20 +29,72 @@ size_t Evaluation::getMobility(const color &pieceColor) const {
   return movgen.getNumberOfMoves();
 };
 // TODO: finish this impolementation;
-int Evaluation::pieceSquareTables(const color &pieceColor) const {
-
-  int pieceSqVal;
-  for (int clr = 0; clr < 2; clr++) {
-    for (int sq = 0; sq < 64; sq++) {
-
-      pieceVal +=
-    }
+// square tables
+int Evaluation::pawnSquareTables(const color &pieceColor) const {
+  int out = 0;
+  int tempSq;
+  uint64_t remainingPawns = position.getPieces()[pieceColor][pawn];
+  while (remainingPawns) {
+    tempSq = __builtin_ctzll(remainingPawns);
+    out += pawnSqTbls[pieceColor][tempSq];
+    remainingPawns ^= (0b1ull << tempSq);
   }
+  return out;
+}
+int Evaluation::knightSquareTables(const color &pieceColor) const {
 
-  return 0;
+  int out = 0;
+  int tempSq;
+  uint64_t remainingKnights = position.getPieces()[pieceColor][knight];
+  while (remainingKnights) {
+    tempSq = __builtin_ctzll(remainingKnights);
+    out += knightSqTbls[pieceColor][tempSq];
+    remainingKnights ^= (0b1ull << tempSq);
+  }
+  return out;
+}
+int Evaluation::queenSquareTables(const color &pieceColor) const {
+
+  int out = 0;
+  int tempSq;
+  uint64_t remainingQueens = position.getPieces()[pieceColor][queen];
+  while (remainingQueens) {
+    tempSq = __builtin_ctzll(remainingQueens);
+    out += queenSqTbls[pieceColor][tempSq];
+    remainingQueens ^= (0b1ull << tempSq);
+  }
+  return out;
+}
+int Evaluation::rookSquareTables(const color &pieceColor) const {
+  int out = 0;
+  int tempSq;
+  uint64_t remainingRooks = position.getPieces()[pieceColor][rook];
+  while (remainingRooks) {
+    tempSq = __builtin_ctzll(remainingRooks);
+    out += rookSqTbls[pieceColor][tempSq];
+    remainingRooks ^= (0b1ull << tempSq);
+  }
+  return out;
+}
+int Evaluation::bishopSquareTables(const color &pieceColor) const {
+  int out = 0;
+  int tempSq;
+  uint64_t remainingBishops = position.getPieces()[pieceColor][bishop];
+  while (remainingBishops) {
+    tempSq = __builtin_ctzll(remainingBishops);
+    out += bishopSqTbls[pieceColor][tempSq];
+    remainingBishops ^= (0b1ull << tempSq);
+  }
+  return out;
+}
+int Evaluation::pieceSquareTables() const {
+  return rookSquareTables(white) - rookSquareTables(black) +
+         pawnSquareTables(white) - pawnSquareTables(black) +
+         bishopSquareTables(white) - bishopSquareTables(black) +
+         queenSquareTables(white) - queenSquareTables(black) +
+         knightSquareTables(white) - knightSquareTables(black);
 }
 float Evaluation::evaluate() const {
-
   int kingDiff = getPieceCount(king, white) - getPieceCount(king, black);
   int queenDiff = getPieceCount(queen, white) - getPieceCount(queen, black);
   int knightDiff = getPieceCount(knight, white) - getPieceCount(knight, black);
@@ -52,12 +104,18 @@ float Evaluation::evaluate() const {
   int doubledPawnDiff = getDoubledPawnCount(white) - getDoubledPawnCount(black);
   int isolinDiff = getIsolatedPawnCount(white) - getIsolatedPawnCount(black);
   size_t mobilityDiff = getMobility(white) - getMobility(black);
+  // std::cout << pieceSquareTables() << std::endl;
   // return (200 * kingDiff + 9 * queenDiff + 5 * rookDiff +
   //         3 * (bishopDiff + knightDiff) + pawnDiff -
   //         0.5 * (isolinDiff + doubledPawnDiff) + 0.1 * (mobilityDiff)) *
   //        whoToMove[position.getTurn()];
+  // std::cout << (200 * kingDiff + 9 * queenDiff + 5 * rookDiff +
+  //               3 * (bishopDiff + knightDiff) + pawnDiff -
+  //               0.5 * (isolinDiff + doubledPawnDiff) + 0 *
+  //               pieceSquareTables())
+  //           << "\n";
   return (200 * kingDiff + 9 * queenDiff + 5 * rookDiff +
           3 * (bishopDiff + knightDiff) + pawnDiff -
-          0.5 * (isolinDiff + doubledPawnDiff)) *
+          0.5 * (isolinDiff + doubledPawnDiff) + pieceSquareTables() / 100) *
          whoToMove[position.getTurn()];
 };
--- chess_game_cpp/tests/evaluationTest.cpp
+++ chess_game_cpp/tests/evaluationTest.cpp
@@ -1,10 +1,11 @@
 #include "evaluation.hpp"
 
 int main() {
-  std::string FEN = "r1b1kb1r/1pqn1ppp/pn6/4P3/3NP1P1/2P1BQ2/PP5P/R3KB1R/";
+  // std::string FEN = "r1b1kb1r/1pqn1ppp/pn6/4P3/3NP1P1/2P1BQ2/PP5P/R3KB1R/";
+  std::string FEN = "rnbqkbnr/1ppppppp/p7/8/3P4/8/PPP1PPPP/RNBQKBNR/";
   Position position(FENtoPieces(FEN), white);
   position.printBoard();
   Evaluation eval(position);
-  std::cout << "Evaluation for this position is" << eval.evaluate()
+  std::cout << "Evaluation for this position is " << eval.evaluate()
             << std::endl;
 }
