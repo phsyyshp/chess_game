bd477cc15f15374d36a1a2c28f33636f961401ac
“physsyhp” <theoriginal169@gmail.com>
2024-01-21 09:58:47 -0500

    a bug in eval is fixed

--- chess_game_cpp/src/evaluation.hpp
+++ chess_game_cpp/src/evaluation.hpp
@@ -9,12 +9,26 @@ public:
   // getters;
   Position getPosition() const;
   int getPieceCount(const piece &, const color &) const;
+  size_t getMobility(const color &pieceColor) const;
+
   // pawn operations;
   int getDoubledPawnCount(const color &) const;
   int getBlockedPawnCount(const color &) const;
   int getIsolatedPawnCount(const color &) const;
 
-  size_t getMobility(const color &) const;
+  int pawnSquareTables(const color &pieceColor) const;
+  // other piece operations
+
+  int knightSquareTables(const color &pieceColor) const;
+
+  int queenSquareTables(const color &pieceColor) const;
+
+  int rookSquareTables(const color &pieceColor) const;
+
+  int bishopSquareTables(const color &pieceColor) const;
+
+  int pieceSquareTables() const;
+
   float evaluate() const;
 
 private:
