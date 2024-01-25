fe04ed3bdf5a95fc7ca595d32a2cbf4be006f409
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-01-25 18:37:12 -0500

    added iterators

--- chess_game_cpp/src/movelist.cpp
+++ chess_game_cpp/src/movelist.cpp
@@ -1,3 +1,4 @@
 #include "movelist.hpp"
 
-void MoveList::pushBack(Move move) { elements[lastSetIdx + 1] = move; }
+void MoveList::push_back(Move move) { elements[lastSetIdx + 1] = move; }
+int MoveList::size() const { return lastSetIdx; }
--- chess_game_cpp/tests/perftTest.cpp
+++ chess_game_cpp/tests/perftTest.cpp
@@ -22,6 +22,7 @@ public:
     }
     return nodes;
   }
+
   int perft(int depth) {
     Position tempPosition;
     if (depth == 0) {
@@ -36,7 +37,6 @@ public:
       if (!position.isInCheck()) {
         nodes += perft(depth - 1);
       }
-
       position = tempPosition;
     }
     return nodes;
@@ -57,19 +57,20 @@ void perftDivide(Position position, int depth) {
     position = tempPosition;
   }
 }
+
 int main() {
   Position position;
   position.setBoardToInitialConfiguration();
   perftTest test(position);
-  int depth = 3;
+  int depth = 5;
   // while (depth > 0) {
-  std::cout << "Number Of Pseudo-Legal Moves:" << std::endl;
-  std::cout << test.perftPseudoLegal(depth) << std::endl;
+  // std::cout << "Number Of Pseudo-Legal Moves:" << std::endl;
+  // std::cout << test.perftPseudoLegal(depth) << std::endl;
 
-  std::cout << "Number Of Legal Moves:" << std::endl;
+  // std::cout << "Number Of Legal Moves:" << std::endl;
   std::cout << test.perft(depth) << std::endl;
-  std::cout << "Divided Perft"
-            << "\n";
+  // std::cout << "Divided Perft"
+  // << "\n";
   // depth--;
   // }
   // Move move(b1, a3, knight, white, false);
