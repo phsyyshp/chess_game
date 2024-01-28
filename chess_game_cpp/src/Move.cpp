732c3faae267e5af2226bfd2551d13f29c035b4c
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-01-27 22:46:33 -0500

    unnecesary bits in move is cleared

--- chess_game_cpp/src/Move.cpp
+++ chess_game_cpp/src/Move.cpp
@@ -1,6 +1,6 @@
 #include "Move.hpp"
 int Move::getFrom() const { return moveNum & 0x3f; }
 int Move::getTo() const { return (moveNum >> 6) & 0x3f; }
-int Move::getColor() const { return (moveNum >> 18) & 0x3f; }
-int Move::getPiece() const { return (moveNum >> 12) & 0x3f; }
-bool Move::checkIsCapture() const { return (moveNum >> 24) == 1; }
+int Move::getColor() const { return (moveNum >> 15) & 0b1; }
+int Move::getPiece() const { return (moveNum >> 12) & 0b111; }
+bool Move::checkIsCapture() const { return (moveNum >> 16) == 1; }
