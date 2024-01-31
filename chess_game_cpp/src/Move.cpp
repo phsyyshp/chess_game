aa54c4732fcd7d3247476cf510a8f6aa74b62fa8
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-01-31 03:30:56 -0500

    score is added to move

--- chess_game_cpp/src/Move.cpp
+++ chess_game_cpp/src/Move.cpp
@@ -4,3 +4,4 @@ int Move::getTo() const { return (moveNum >> 6) & 0x3f; }
 int Move::getColor() const { return (moveNum >> 15) & 0b1; }
 int Move::getPiece() const { return (moveNum >> 12) & 0b111; }
 bool Move::checkIsCapture() const { return (moveNum >> 16) == 1; }
+int Move::getScore() const { return (moveNum) >> 17; }
