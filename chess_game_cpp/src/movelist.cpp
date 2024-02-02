a6a3e3b50a4e683d9a8f60141bca479c0d865a8b
“physsyhp” <theoriginal169@gmail.com>
2024-02-02 17:58:37 -0500

    getcaptured moves

--- chess_game_cpp/src/movelist.cpp
+++ chess_game_cpp/src/movelist.cpp
@@ -11,3 +11,13 @@ void MoveList::swap(int i, int j) {
   elements[j] = elements[i];
   elements[i] = temp;
 }
+// TODO: possible optimizations here.
+MoveList MoveList::getCapturedMoves() const {
+  MoveList captureMoves;
+  for (int i = 0; i < (lastSetIdx + 1); i++) {
+    if (elements[i].checkIsCapture()) {
+      captureMoves.push_back(elements[i]);
+    }
+  }
+  return captureMoves;
+}
