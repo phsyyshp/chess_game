b3c491983db1368421dbe34c8be1ddd01f5286e6
“physsyhp” <theoriginal169@gmail.com>
2024-02-02 02:38:48 -0500

    score bug is found

--- chess_game_cpp/src/Move.cpp
+++ chess_game_cpp/src/Move.cpp
@@ -1,5 +1,6 @@
 #include "Move.hpp"
 #include "position.hpp"
+// getters
 int Move::getFrom() const { return moveNum & 0x3f; }
 int Move::getTo() const { return (moveNum >> 6) & 0x3f; }
 int Move::getColor() const { return (moveNum >> 15) & 0b1; }
@@ -9,6 +10,16 @@ int Move::getScore() const { return (moveNum) >> 17; }
 piece Move::getCaptured(const Position &position) const {
   return position.getMailbox()[getTo()];
 }
+// setters
 // Warning! this does not override the already set score of a move. Must be used
 // only if the score of the move is not yet given.
 void Move::setScore(const int &score) { moveNum |= score << 17; }
+// visualizers
+void Move::print() const {
+
+  std::cout << "move: " << chessSq[getFrom()] << chessSq[getTo()]
+            << "color: " << getColor()
+            << "piece: " << pieceToStr(static_cast<piece>(getPiece()))
+            << "is capture: " << checkIsCapture() << "score: " << getScore()
+            << std::endl;
+}
