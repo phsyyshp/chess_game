43c39e8075923d421a65be0bd2c30e35aabda7c0
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-02-02 02:52:29 -0500

    nasty bit manipulation bug in Move is found

--- chess_game_cpp/src/Move.cpp
+++ chess_game_cpp/src/Move.cpp
@@ -5,7 +5,7 @@ int Move::getFrom() const { return moveNum & 0x3f; }
 int Move::getTo() const { return (moveNum >> 6) & 0x3f; }
 int Move::getColor() const { return (moveNum >> 15) & 0b1; }
 int Move::getPiece() const { return (moveNum >> 12) & 0b111; }
-bool Move::checkIsCapture() const { return (moveNum >> 16) == 1; }
+bool Move::checkIsCapture() const { return (moveNum >> 16) & 0x1; }
 int Move::getScore() const { return (moveNum) >> 17; }
 piece Move::getCaptured(const Position &position) const {
   return position.getMailbox()[getTo()];
@@ -16,7 +16,6 @@ piece Move::getCaptured(const Position &position) const {
 void Move::setScore(const int &score) { moveNum |= score << 17; }
 // visualizers
 void Move::print() const {
-
   std::cout << "move: " << chessSq[getFrom()] << chessSq[getTo()]
             << "color: " << getColor()
             << "piece: " << pieceToStr(static_cast<piece>(getPiece()))
