720c0b6ecde41a94558e7f011fe38a50e4545630
“physsyhp” <theoriginal169@gmail.com>
2024-02-04 21:21:39 -0500

    several refactoring and killer move

--- chess_game_cpp/src/search.hpp
+++ chess_game_cpp/src/search.hpp
@@ -1,21 +1,27 @@
 #include "evaluation.hpp"
 class Search {
 public:
+  // constructors;
   Search() = default;
   Search(const Position &p) : position(p){};
-
+  // getters;
+  std::array<std::array<Move, MAX_DEPTH>, MAX_KILLER_MOVES>
+  getKillerMoves() const;
+  int getPly() const;
+  // searchers;
   int negaMax(int depth);
   Move search(int depth);
   Move searchAB(int depth);
   int quiesce(int alpha, int beta);
+  int alphaBeta(int alpha, int beta, int depthLeft);
+  // move ordering;
   void scoreMoves(MoveList &moveList_) const;
   void pickMove(MoveList &moveList_, int startingIdx) const;
   void orderMoves(MoveList &movelist_);
-  int alphaBeta(int alpha, int beta, int depthLeft);
   void storeKillerMove(const Move &move_, int ply);
 
 private:
   Position position;
-  std::array<std::array<Move, 64>, 2> killerMoves;
-  // int ply;
+  std::array<std::array<Move, MAX_DEPTH>, MAX_KILLER_MOVES> killerMoves;
+  int ply = 0;
 };
