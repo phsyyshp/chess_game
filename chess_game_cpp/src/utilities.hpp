7f2de0bc900e16eac75e979d867f657c9135c4d1
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-02-05 21:30:50 -0500

    findindex is moved to here from gamelooptest

--- chess_game_cpp/src/utilities.hpp
+++ chess_game_cpp/src/utilities.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "constants.hpp"
 #include <array>
 #include <random>
@@ -13,3 +14,8 @@ int chartoColor(char c);
 std::array<std::array<uint64_t, 6>, 2> FENtoPieces(std::string FENstring);
 uint64_t pseudoRandomNumberGenerator();
 std::array<uint64_t, 781> generateZobristTable();
+template <typename T, size_t N>
+int findIndex(const std::array<T, N> &arr, const T &value) {
+  auto it = std::find(arr.begin(), arr.end(), value);
+  return (it != arr.end()) ? std::distance(arr.begin(), it) : -1;
+}
