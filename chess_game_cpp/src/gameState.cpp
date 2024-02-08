ba738e5dd2be3db14a28397f5b2d90f2d0a03323
“physsyhp” <theoriginal169@gmail.com>
2024-02-07 21:59:21 -0500

    enpassant bits are fixed

--- chess_game_cpp/src/gameState.cpp
+++ chess_game_cpp/src/gameState.cpp
@@ -5,7 +5,7 @@ color GameState::getTurn() const {
   return static_cast<color>(gameStateNum & 0x1);
 }
 uint GameState::getCastlingRigths() const { return (gameStateNum >> 1) & 0xf; }
-uint GameState::getEnPassant() const { return (gameStateNum >> 5) & 0xf; }
+uint GameState::getEnPassant() const { return (gameStateNum >> 5) & 0x7; }
 uint32_t GameState::getGameStateNum() const { return gameStateNum; }
 // setters
 void GameState::changeTurn() { gameStateNum ^= 0x1; }
