a675175153da28a2cbb7ad6f3fb562c44fafa57d
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-02-07 22:01:07 -0500

    castling table

--- chess_game_cpp/src/gameState.hpp
+++ chess_game_cpp/src/gameState.hpp
@@ -9,10 +9,16 @@ public:
       : gameStateNum((turn & 0x1) | ((0b1111u << 1) & 0xf) |
                      ((0b0u << 5) & 0x7)){};
 
-  // canWhiteCastleQueenSide, 1 0 0 0
-  // canWhiteCastleKingSide,  0 1 0 0
-  // canBlackCastleQueenSide, 0 0 1 0
-  // canBlackCastleKingSide,  0 0 0 1
+  // Castling Rights Table
+  // +-----------------------+---------------------+
+  // |       Permission      |      Castling       |
+  // +-----------------------+---------------------+
+  // | canWhiteCastleQueenSide | 1 0 0 0 |
+  // | canWhiteCastleKingSide  | 0 1 0 0 |
+  // | canBlackCastleQueenSide | 0 0 1 0 |
+  // | canBlackCastleKingSide  | 0 0 0 1 |
+  // +-----------------------+---------------------+
+
   GameState(color turn, const uint &castlingRigths, const uint &enPassant)
       : gameStateNum((turn & 0x1) | ((castlingRigths << 1) & 0xf) |
                      ((enPassant << 5) & 0x7)) {}
