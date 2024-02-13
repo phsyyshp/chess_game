a5e0239b60b3da60a763d01aa93f80e126b0c81a
“physsyhp” <theoriginal169@gmail.com>
2024-02-13 09:59:07 -0500

    loader bug fixes

--- chess_game_cpp/src/loader.hpp
+++ chess_game_cpp/src/loader.hpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <unistd.h>
+#include <limits.h>
 #ifndef LOADER_HPP
 #define LOADER_HPP
 std::string pieceToStr(piece pieceType);
@@ -10,4 +12,6 @@ std::vector<uint64_t> lineToNumsVec(const std::string &line);
 std::vector<uint64_t> fileToVec(std::string fileName);
 std::vector<std::vector<uint64_t>> fileToVec2(std::string fileName);
 std::vector<magicTbls> fileToLookUpsVec(piece pieceType);
+std::string getExecutableDirectory() ;
+ 
 #endif // LOADER_HPP
