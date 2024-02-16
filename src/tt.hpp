812b963dff2a7449ed581fafe99bb04aabb453bb
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-02-16 15:06:11 -0500

    move type is removed

--- src/tt.hpp
+++ src/tt.hpp
@@ -1,5 +1,13 @@
 #pragma once
+#include "Move.hpp"
 #include "constants.hpp"
+struct hashEntry {
+  uint64_t zobrist;
+  int depth;
+  int score;
+  int ancient;
+  Move move;
+};
 class TranspositionTable {
   static std::array<hashEntry, 100> tt;
 
