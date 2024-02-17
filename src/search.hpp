70a4e5e34c9fd531e2461cc7ac4e0c7f653d7f1f
“physsyhp” <theoriginal169@gmail.com>
2024-02-17 04:40:35 -0500

    double copy is removed

--- src/search.hpp
+++ src/search.hpp
@@ -5,9 +5,8 @@ class Search {
 public:
   // constructors;
   Search() = default;
-  Search(const Position &p);
-  Search(const Position &p, int timeLeftWhite_, int timeIncrementWhite_,
-         int timeLeftBlack_, int timeIncrementBlack_);
+  Search(int timeLeftWhite_, int timeIncrementWhite_, int timeLeftBlack_,
+         int timeIncrementBlack_);
 
   // getters;
   std::array<std::array<Move, MAX_DEPTH>, MAX_KILLER_MOVES>
@@ -15,15 +14,15 @@ public:
   int getPly() const;
 
   // searchers;
-  int negaMax(int depth);
-  Move search(int depth);
-  Move searchIt(int maxDepth, bool isInfoOn);
+  int negaMax(int depth, const Position &position);
+  Move search(int depth, const Position &position);
+  Move searchIt(int maxDepth, bool isInfoOn, const Position &position);
   Move searchAB(int depth, std::chrono::high_resolution_clock::time_point start,
-                int remainingTime, int timeIncrement);
-  int quiesce(int alpha, int beta);
-  int alphaBeta(int alpha, int beta, int depthLeft);
+                int remainingTime, int timeIncrement, const Position &position);
+  int quiesce(int alpha, int beta, const Position &position);
+  int alphaBeta(int alpha, int beta, int depthLeft, const Position &position);
   // move ordering;
-  void scoreMoves(MoveList &moveList_) const;
+  void scoreMoves(MoveList &moveList_, const Position &position) const;
   void pickMove(MoveList &moveList_, int startingIdx) const;
   void orderMoves(MoveList &movelist_);
   void storeKillerMove(const Move &move_, int ply);
@@ -32,7 +31,6 @@ public:
   int countTime(std::chrono::high_resolution_clock::time_point);
 
 private:
-  Position position;
   std::array<std::array<Move, MAX_DEPTH>, MAX_KILLER_MOVES> killerMoves;
   int ply = 0;
   int timeLeftWhite;
